42458e33d43809cbc3f7a54c1c11fbe075972138
Craig Murray <cmurray4492@gmail.com>
2026-05-16 12:39:33 -0500

    first commit

---
+++ cmd/rag/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// We need to:
+	// - Setup the app
+	// - Setup config
+	// - Setup an LLM client
+	// - Setup the read-eval-print loop (REPL)
+	// -
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := app.Run(ctx, config.load()); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+}
